a1d9d1c2dc9f14be24756cd29a25cfd05bdc6958
andiLeong <alwayssmile20150724@gmail.com>
2022-06-16 14:28:27 +0800

    finish home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
                 <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 dark:text-gray-300 sm:text-xl md:mt-5 md:max-w-3xl"> Is the best provider of residential communities in the Philippines</p>
                 <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
                     <div class="rounded-md shadow">
-                        <a href="https://www.dmcihomes.com/" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 md:py-4 md:text-lg md:px-10"> Buy Now </a>
+                        <AppLink to="offer" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 md:py-4 md:text-lg md:px-10"> Buy Now </AppLink>
                     </div>
 <!--                    <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">-->
 <!--                        <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"> Live demo </a>-->
@@ -27,13 +27,12 @@
     <div class="relative bg-white dark:bg-gray-700 py-16 sm:py-24 lg:py-32">
         <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
             <h2 class="text-base font-semibold uppercase tracking-wider text-sky-600">Affordable price</h2>
-            <p class="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-300 sm:text-4xl">Everything you need to deploy your app</p>
-            <p class="mx-auto mt-5 max-w-prose text-xl text-gray-500">Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.</p>
+            <p class="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-300 sm:text-4xl">Everything you need to know how great DMCI is</p>
+            <p class="mx-auto mt-5 max-w-prose text-xl text-gray-500">From flexible payment tern to convenient accessible location, you get regret if you choose other condo developer</p>
             <div class="mt-12">
                 <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                    <div class="pt-6" v-for="feature in features" :ke="feature.name">
-                        <div class="flow-root rounded-lg bg-gray-50 dark:bg-gray-600 px-6 pb-8">
-
+                    <div class="pt-6" v-for="feature in features" :key="feature.name">
+                        <div class="flow-root rounded-lg bg-gray-50 dark:bg-gray-600 px-6 pb-8 h-56">
                             <div class="-mt-6" >
                                 <div>
                 <span class="inline-flex items-center justify-center rounded-md bg-sky-500 p-3 shadow-lg">
@@ -43,8 +42,8 @@
                   </svg>
                 </span>
                                 </div>
-                                <h3 class="mt-8 text-lg font-medium tracking-tight text-gray-900 dark:text-white">{{feature.name}}</h3>
-                                <p class="mt-5 text-base text-gray-500 dark:text-gray-200">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                                <h3 class="mt-6 text-lg font-medium tracking-tight text-gray-900 dark:text-white">{{feature.name}}</h3>
+                                <p class="mt-3 text-base text-gray-500 dark:text-gray-200">{{feature.description}}</p>
                             </div>
 
                         </div>
@@ -59,8 +58,8 @@
     <div class="bg-gray-200 dark:bg-gray-700  pt-12 sm:pt-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="max-w-4xl mx-auto text-center">
-                <h2 class="text-3xl font-extrabold text-gray-900 dark:text-gray-200 sm:text-4xl">Trusted by developers from over 80 planets</h2>
-                <p class="mt-3 text-xl text-gray-500 sm:mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.</p>
+                <h2 class="text-3xl font-extrabold text-gray-900 dark:text-gray-200 sm:text-4xl">Properties All Around Philippines</h2>
+                <p class="mt-3 text-xl text-gray-500 sm:mt-4">With a total of 76 projects, 61 turned over, a 80.2% turnover rate, it gain a lot of customer's trust </p>
             </div>
         </div>
         <div class="mt-10 pb-12  sm:pb-16">
@@ -70,16 +69,16 @@
                     <div class="max-w-4xl mx-auto">
                         <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
                             <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Pepperoni</dt>
-                                <dd class="order-1 text-5xl font-extrabold text-sky-600"> <CountUp to="25894"/></dd>
+                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Total Property</dt>
+                                <dd class="order-1 text-5xl font-extrabold text-sky-600"> <CountUp to="76"/></dd>
                             </div>
                             <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Delivery</dt>
-                                <dd class="order-1 text-5xl font-extrabold text-sky-600"> <CountUp to="5869" /></dd>
+                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Delivery Property</dt>
+                                <dd class="order-1 text-5xl font-extrabold text-sky-600"> <CountUp to="61" /></dd>
                             </div>
                             <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Calories</dt>
-                                <dd class="order-1 text-5xl font-extrabold text-sky-600"><CountUp to="887" /></dd>
+                                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Under Construction Property</dt>
+                                <dd class="order-1 text-5xl font-extrabold text-sky-600"><CountUp to="12" /></dd>
                             </div>
                         </dl>
                     </div>
@@ -96,12 +95,12 @@ import CountUp from "../components/CountUp.vue";
 import {ref} from "vue";
 
 const features = ref([
-    {name: 'Flexible payment term'},
-    {name: 'Excellent quality'},
-    {name: 'Excellent quality'},
-    {name: 'Spacious space'},
-    {name: 'Modern city life'},
-    {name: 'Theme Development'},
+    {name: 'Flexible payment term',description:'DMCI offer a very flexible payment tern, some allow you to pay down-payment that stretch in few years'},
+    {name: 'Excellent quality', description:'it has some property that are built back in early 2000, are still with thousand of residents, and you know how awesome is the construction quality'},
+    {name: 'High Turnover Rate', description: 'Currently about 80% projects that are turn over, you are not afraid to lost any of your money'},
+    {name: 'Spacious space',description: 'DMCI comes with mostly at least 2br, only few 1br you may find'},
+    {name: 'Modern city life', description:'With each property located strategically convenient to city life, giving you easy access to mall ,school, hospital'},
+    {name: 'Theme Development', description: 'Some projects that are design based on some theme, like kia garden residence based on Japan theme, definitely feel like living in Japan '},
 ])
 
 </script>
